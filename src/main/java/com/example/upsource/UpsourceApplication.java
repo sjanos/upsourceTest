6d7426f2d561d920be9c1efd1ff871428f0f167d
Janos Sechna <jsechna@cisco.com>
2019-04-02 16:50:15 +0100

    Initial commit

---
+++ src/main/java/com/example/upsource/UpsourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.upsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpsourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpsourceApplication.class, args);
+    }
+
+}
