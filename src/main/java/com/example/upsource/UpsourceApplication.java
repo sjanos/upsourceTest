098642dd59f46bdacb5990a19fec129c03104ab7
Janos Sechna <s.janospal@gmail.com>
2019-04-07 23:47:39 +0100

    add new bean

--- src/main/java/com/example/upsource/UpsourceApplication.java
+++ src/main/java/com/example/upsource/UpsourceApplication.java
@@ -2,12 +2,18 @@ package com.example.upsource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UpsourceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UpsourceApplication.class, args);
+    }
+
+    @Bean
+    public String stringBean(){
+        return "BEAN";
     }
 
 }
