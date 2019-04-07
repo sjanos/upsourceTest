edd858bace103ebcc481ff29f927502b40f297b7
Janos Sechna <s.janospal@gmail.com>
2019-04-07 23:43:41 +0100

    add new controller

--- src/main/java/com/example/upsource/UpsourceApplication.java
+++ src/main/java/com/example/upsource/UpsourceApplication.java
@@ -2,6 +2,8 @@ package com.example.upsource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class UpsourceApplication {
@@ -10,4 +12,12 @@ public class UpsourceApplication {
         SpringApplication.run(UpsourceApplication.class, args);
     }
 
+    @RestController
+    public static class SimpleController {
+
+        @GetMapping
+        public String hello() {
+            return "Hello";
+        }
+    }
 }
